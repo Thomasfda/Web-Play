e83f2cb43ab8cb8dbcee20a032208a8b7e1bcac7
Thomasfda <thomasfda@outlook.fr>
2016-05-07 15:56:01 +0200

    Added files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'web-play-beta',
+		wp_ws_url : 'http://actualites.web-play.hmsw.fr/wp-appkit-api/web-play-beta',
+		theme : 'wpak-tabs-themes-master',
+		version : '0.1',
+		app_title : 'Web-Play (beta)',
+		app_platform : 'android',
+		gmt_offset : 2,
+		debug_mode : 'off',
+		auth_key : 'R*(@SZAd@Lh#0HMA^P1FoghmMQ(R9pAU17sM!apmTWjNQa6(hkE35KdKobmCL8H@',
+		options : {"refresh_interval":0},
+		addons : []
+	};
+
+});
